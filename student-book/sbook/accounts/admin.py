e6a0cd8cb4edca36506f34632ac45e31fcb592ef
urbrob <35544390+urbrob@users.noreply.github.com>
2019-03-22 23:35:40 +0100

    Add Quiz field (#3)

--- student-book/sbook/accounts/admin.py
+++ student-book/sbook/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from accounts.models import User, Group, Membership
 
-# Register your models here.
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display_links = ['pk']
+    list_display = ('pk','user', 'status', 'group', 'created_at')
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display_links = ['pk']
+    list_display = ('pk','name', 'created_at', 'created_by')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display_links = ['pk']
+    list_display = ('pk','first_name', 'last_name', 'username')
--- student-book/sbook/notes/models.py
+++ student-book/sbook/notes/models.py
@@ -7,19 +7,19 @@ from sbook.utils import current_user
 class Quiz(StringIdModel):
     name = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name="quizes", default=current_user)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizes",  default=current_user)
 
 
 class Question(StringIdModel):
     description = models.CharField(max_length=250)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name="questions", default=current_user)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="questions",  default=current_user)
 
 
 class Answer(StringIdModel):
     description = models.CharField(max_length=250)
     is_valid = models.BooleanField(default=False)
-    question = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name="answers", default=current_user)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="answers",  default=current_user)
