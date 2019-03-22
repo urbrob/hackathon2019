09f7c9877e0a28747e98aa8a2ee2a29f44839d61
urbrob <35544390+urbrob@users.noreply.github.com>
2019-03-22 23:46:09 +0100

    Add basic template (#4)

--- student-book/sbook/notes/urls.py
+++ student-book/sbook/notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("question/<quiz_pk>", views.QuestionList.as_view(), name="quiz_read"),
+    path("question_display/<quiz_pk>", views.quiz_display, name="quiz_display"),
 ]
--- student-book/sbook/notes/views.py
+++ student-book/sbook/notes/views.py
@@ -4,6 +4,20 @@ from notes.models import Quiz
 from rest_framework.renderers import JSONRenderer
 from notes.serializers import QuizSerializer
 from rest_framework.response import Response
+from django.template.loader import get_template
+from django.http import HttpResponse
+import datetime
+
+
+def quiz_display(request, quiz_pk):
+    try:
+        quiz = Quiz.objects.get(id=quiz_pk)
+    except Quiz.DoesNotExist:
+        return Response({})
+    quiz_serializer = QuizSerializer(quiz)
+    html = get_template('quiz_display.html').render({'quiz': quiz_serializer.data})
+    return HttpResponse(html)
+
 
 
 class QuestionList(APIView):
