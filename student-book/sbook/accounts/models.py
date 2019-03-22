0cf87835c250d8181c5b3f82c46536d13e0448ee
Robert <urb.rob@o2.pl>
2019-03-22 21:17:43 +0100

    Fix users

--- student-book/sbook/accounts/models.py
+++ student-book/sbook/accounts/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from sbook.models import StringIdModel
 
 
-class CustomUser(StringIdModel, AbstractUser):
+class User(StringIdModel, AbstractUser):
     pass
