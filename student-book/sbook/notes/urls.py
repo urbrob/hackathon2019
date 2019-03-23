ffbcd0c16426652d7531d006d9af23514fb04f94
Robert <urb.rob@o2.pl>
2019-03-23 18:58:09 +0100

    change name

--- student-book/sbook/notes/urls.py
+++ student-book/sbook/notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("question/<quiz_pk>", views.QuestionList.as_view(), name="quiz_read"),
+    path("quiz/<quiz_pk>", views.QuestionList.as_view(), name="quiz_read"),
     path("group/<group_pk>/<quiz_pk>/manage", views.quiz_display, name="quiz_display"),
     path("group/<group_pk>", views.group_display, name='group_display'),
 
